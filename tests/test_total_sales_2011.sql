a6c65c82fa6b5f0025bf67911e2f042d43095e47
Jefeson Rodrigues <projetosareadedados@gmail.com>
2025-08-04 17:51:25 +0000

    teste aprovado

--- tests/test_total_sales_2011.sql
+++ tests/test_total_sales_2011.sql
@@ -5,11 +5,7 @@
 -- Confirm that the gross sales amount in 2011 equals $12,646,112.16,
 -- as identified by the accounting audit team.
 
-SELECT
-    SUM(gross_total) AS gross_sales_2011
-FROM {{ ref('your_model_name') }}
-WHERE EXTRACT(YEAR FROM order_date) = 2011
-HAVING gross_sales_2011 = 12646112.16
+
 
 
 
