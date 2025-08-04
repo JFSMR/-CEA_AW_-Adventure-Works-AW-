2a7ab66a1f224f2bb6d79c4ec6d4ab977e5b6f9d
Jefeson Rodrigues <projetosareadedados@gmail.com>
2025-08-04 01:14:57 +0000

    erro bug corrigido

--- models/intermediate/int_sales_reason_enriched.sql
+++ models/intermediate/int_sales_reason_enriched.sql
@@ -1,35 +1,32 @@
-
-
 with salesreason as (
     select 
-          sales_order_pk
-        , sales_reason_fk
+        sales_order_pk
+      , sales_reason_fk
     from {{ ref('stg_erp__sales_salesorderheadersalesreason') }}
-),
+)
 
-reason as (
+, reason as (
     select 
-          sales_reason_pk
-        , name_reason
-        , type_reason
+        sales_reason_pk
+      , name_reason
+      , type_reason
     from {{ ref('stg_erp__SALES_SALESREASON') }}
-),
+)
 
-joined as (
+, joined as (
     select 
-         r.sales_reason_pk
-        , s.sales_order_pk as sales_order_id            
-        , r.name_reason
-        , r.type_reason
+        r.sales_reason_pk
+      , s.sales_order_pk as sales_order_id            
+      , r.name_reason
+      , r.type_reason
     from salesreason s
     left join reason r
         on s.sales_reason_fk = r.sales_reason_pk
 )
 
 select
-      sales_reason_pk
-     , sales_order_id    
-    , name_reason
-    , type_reason
+    sales_reason_pk
+  , sales_order_id    
+  , name_reason
+  , type_reason
 from joined
-
