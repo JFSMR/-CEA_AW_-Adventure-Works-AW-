87a2857086dff8fd4c84be3a7a7517aa3566def8
Jefeson Rodrigues <projetosareadedados@gmail.com>
2025-08-10 12:59:47 +0000

    Acrescentando o  ship_date

--- models/staging/erp/stg_erp__sales_salesorderheader.sql
+++ models/staging/erp/stg_erp__sales_salesorderheader.sql
@@ -14,29 +14,29 @@ SELECT
     ,CAST(territoryid AS INT)                    AS territory_fk               
     ,CAST(billtoaddressid AS INT)                AS address_fk              
     ,CAST(creditcardid AS INT)                   AS creditcard_fk 
-    ,CAST(salespersonid AS INT)                  AS salesperson_fk                
+    ,CAST(salespersonid AS INT)                  AS salesperson_fk          
     ,CAST(status AS INT)                         AS status 
     ,CAST(subtotal AS NUMERIC(18,4))             AS sub_total                     
     ,CAST(totaldue AS NUMERIC(18,4))             AS total_due                 
     ,CAST(orderdate AS DATE)                     AS order_date 
-    ,CAST(freight AS NUMERIC(18,4))             AS freight                   
+    ,CAST(shipdate AS DATE)                     AS ship_date 
+    ,CAST(freight AS NUMERIC(18,4))              AS freight                   
 
     -- Colunas não necessárias para responder às perguntas de negócio
-    --                    
-    -- ,CAST(taxamt AS NUMERIC(18,2))              AS taxamt                    
-    -- ,CAST(shiptoaddressid AS INT)               AS shiptoaddress_fk       
-    -- ,CAST(shipmethodid AS INT)                  AS shipmethod_fk      
-    -- ,CAST(currencyrateid AS INT)                AS currencyrate_fk   
-    -- ,CAST(revisionnumber AS INT)                AS revisionnumber       
-    -- ,CAST(duedate AS DATE)                      AS due_date                  
-    -- ,CAST(shipdate AS DATE)                     AS ship_date            
-    -- ,CAST(modifieddate AS DATE)                 AS modifie_ddate             
-    -- ,CAST(onlineorderflag AS BOOLEAN)           AS online_order_flag        
-    -- ,CAST(purchaseordernumber AS VARCHAR)       AS purchase_order_number      
-    -- ,CAST(accountnumber AS VARCHAR)             AS account_number         
-    -- ,CAST(creditcardapprovalcode AS VARCHAR)    AS credit_card_approval_code 
-    -- ,CAST(rowguid AS VARCHAR)                   AS rowguid             
-    -- ,CAST(comment AS VARCHAR)                   AS comment                
+                       
+    --,CAST(taxamt AS NUMERIC(18,2))              AS taxamt                    
+    --,CAST(shiptoaddressid AS INT)               AS shiptoaddress_fk       
+    --,CAST(shipmethodid AS INT)                  AS shipmethod_fk      
+    --,CAST(currencyrateid AS INT)                AS currencyrate_fk   
+    --,CAST(revisionnumber AS INT)                AS revisionnumber       
+    --,CAST(duedate AS DATE)                      AS due_date              
+    --,CAST(modifieddate AS DATE)                 AS modifie_ddate             
+    --,CAST(onlineorderflag AS BOOLEAN)           AS online_order_flag        
+    --,CAST(purchaseordernumber AS VARCHAR)       AS purchase_order_number      
+    --,CAST(accountnumber AS VARCHAR)             AS account_number         
+    --,CAST(creditcardapprovalcode AS VARCHAR)    AS credit_card_approval_code 
+    --,CAST(rowguid AS VARCHAR)                   AS rowguid             
+    --,CAST(comment AS VARCHAR)                   AS comment                
        
 FROM source
 
